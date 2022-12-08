87ce372473d9da40fc136eef8116b3f41e8e10f4
Bk0771 <bhailmariam@gmail.com>
2022-12-08 08:14:44 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,12 +10,16 @@
  *
  * * Return: 0
  *
- */
+ * */
 
 
 
- int main(void)
- }
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
- }
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
