04791d41a3cae6eb0d15c37647da95a3e8786868
Bk0771 <117773467+Bk0771@users.noreply.github.com>
2022-12-13 13:36:44 -0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,45 +1,27 @@
 #include "main.h"
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * Return:void
+ * print_sign - check numers
+ * @n : number to check
+ * Return:0 or 1
  */
-void jack_bauer(void)
+int print_sign(int n)
 {
-	int h1;
-	int h2;
-	int m1;
-	int m2;
-	int a = 9;
+	int test;
 
-	h2 = 0;
-	while (h2 <= 2)
+	if (n > 0)
 	{
-		if (h2 == 2)
-		{
-			/*Restrain to 23h, not 29*/
-			a = 3;
-		}
-		h1 = 0;
-		while (h1 <= a)
-		{
-			m2 = 0;
-			while (m2 <= 5)
-			{
-				m1 = 0;
-				while (m1 <= 9)
-				{
-					_putchar('0' + h2);
-					_putchar('0' + h1);
-					_putchar(':');
-					_putchar('0' + m2);
-					_putchar('0' + m1);
-					_putchar('\n');
-					m1++;
-				}
-				m2++;
-			}
-			h1++;
-		}
-		h2++;
+		test = 1;
+		_putchar('+');
 	}
+	else if (n == 0)
+	{
+		test = 0;
+		_putchar('0');
+	}
+	else
+	{
+		test = -1;
+		_putchar('-');
+	}
+	return (test);
 }
